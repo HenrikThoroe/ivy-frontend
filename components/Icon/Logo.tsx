1ff32e5c86dfef35bd78862f0dbe3d405f7a3f5d
Henrik Thoroe <henrik@thoroee.com>
2023-06-08 22:36:10 +0200

    feat(comp): add wrapper for SVG logo

---
+++ components/Icon/Logo.tsx
@@ -0,0 +1,20 @@
+export default function Logo() {
+  return (
+    <svg width="52" height="52" viewBox="0 0 52 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect x="1" y="1" width="50" height="50" fill="#D9D9D9" />
+      <rect x="2" y="2" width="24" height="24" fill="black" />
+      <rect x="2" y="2" width="24" height="24" stroke="black" />
+      <rect x="26" y="2" width="24" height="24" fill="white" />
+      <rect x="26" y="2" width="24" height="24" stroke="black" />
+      <rect x="2" y="26" width="24" height="24" fill="white" />
+      <rect x="2" y="26" width="24" height="24" stroke="black" />
+      <rect x="26" y="26" width="24" height="24" fill="black" />
+      <rect x="26" y="26" width="24" height="24" stroke="black" />
+      <path
+        d="M20.8672 13.369C21.2054 13.129 21.6099 13 22.0247 13H30.6129L34.6763 14.0814C36.2715 14.506 37.4421 15.8668 37.6234 17.5075L39 29.9643L33.129 25.5L27.2581 38L20.5484 26.3929L13 29.9643L15.9057 18.3643C16.1956 17.2073 16.8894 16.192 17.8621 15.5017L20.8672 13.369Z"
+        fill="#3C7023"
+      />
+      <rect x="1" y="1" width="50" height="50" stroke="black" stroke-width="2" />
+    </svg>
+  )
+}
