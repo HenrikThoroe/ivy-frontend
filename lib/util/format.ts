40aa880db6ed906b77ed169eaf14df9d426f7300
Henrik Thoroe <henrik@thoroee.com>
2023-06-14 09:01:14 +0200

    feat(lib): add util to format memory size

---
+++ lib/util/format.ts
@@ -0,0 +1,7 @@
+export function formatMemSize(bytes: number, decimals: number = 2) {
+  const k = 1000
+  const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+  const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(decimals))} ${units[i]}`
+}
