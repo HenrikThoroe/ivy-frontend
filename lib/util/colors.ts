5f85175592dac09ce209973bb31a344b5b1bb459
Henrik Thoroe <henrik@thoroee.com>
2023-06-14 09:00:42 +0200

    feat(lib): add color utilities

---
+++ lib/util/colors.ts
@@ -0,0 +1,20 @@
+export function chartColor(index: number, total: number) {
+  const s = 62
+  const l = 60
+  const h = Math.floor((index / total) * 360)
+
+  return hslToHex(h, s, l)
+}
+
+function hslToHex(h: number, s: number, l: number) {
+  l /= 100
+  const a = (s * Math.min(l, 1 - l)) / 100
+  const f = (n: number) => {
+    const k = (n + h / 30) % 12
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1)
+    return Math.round(255 * color)
+      .toString(16)
+      .padStart(2, '0')
+  }
+  return `#${f(0)}${f(8)}${f(4)}`
+}
