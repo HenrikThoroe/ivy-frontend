30ba9ee8c5c98e6e08c166d5da4cc036d6b58334
Henrik Thoroe <henrik@thoroee.com>
2023-06-14 09:13:55 +0200

    feat(comp): use `List` component

---
+++ components/EngineList/EngineList.tsx
@@ -0,0 +1,27 @@
+import { EngineConfig, getInstances } from '@/lib/data/Engine'
+import EngineListRow from './EngineListRow'
+import List from '../List/List'
+
+interface Props {
+  engine: EngineConfig
+}
+
+export default function EngineList(props: Props) {
+  const { engine } = props
+
+  return (
+    <List variant="custom-1" head={['Version', 'OS', 'Architecture', 'Required Capabilities']}>
+      {getInstances(engine).map((instance) => (
+        <EngineListRow
+          engine={engine.name}
+          id={instance.flavour.id}
+          arch={instance.flavour.arch}
+          os={instance.flavour.os}
+          capabilities={instance.flavour.capabilities}
+          version={instance.version}
+          key={instance.flavour.id}
+        />
+      ))}
+    </List>
+  )
+}
---
+++ components/EngineList/EngineListRow.tsx
@@ -0,0 +1,74 @@
+'use client'
+
+import { EngineVersion, deleteEngineConfig } from '@/lib/data/Engine'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import LoadingModal from '../Modal/LoadingModal'
+import ListRow from '../List/ListRow'
+import ListActions from '../List/ListActions'
+import WithModal from '../Modal/WithModal'
+import ListAction from '../List/ListAction'
+import ActionModal from '../Modal/ActionModal'
+
+interface Props {
+  id: string
+  version: EngineVersion
+  arch: string
+  os: string
+  capabilities: string[]
+  engine: string
+}
+
+export default function EngineListRow(props: Props) {
+  const router = useRouter()
+  const [showLoading, setShowLoading] = useState(false)
+
+  const handleDelete = async () => {
+    setShowLoading(true)
+
+    const didRemoveEngine = await deleteEngineConfig(props.engine, props.id)
+
+    if (didRemoveEngine) {
+      router.push('/engines')
+    } else {
+      router.refresh()
+    }
+  }
+
+  return (
+    <>
+      <LoadingModal open={showLoading} />
+      <ListRow variant="custom-1">
+        <span>{`v${props.version.major}.${props.version.minor}.${props.version.patch}`}</span>
+        <span>{props.os}</span>
+        <span>{props.arch}</span>
+        <span>{props.capabilities.join(', ')}</span>
+        <ListActions>
+          <WithModal
+            modal={
+              <ActionModal
+                title="Delete Engine"
+                description="Deleting an engine will remove it from the registry. This action cannot be undone."
+                icon="delete"
+                action={{
+                  label: 'Delete',
+                  onClick: handleDelete,
+                  variant: 'danger',
+                }}
+              />
+            }
+          >
+            <ListAction variant="action" style="danger" icon="delete" />
+          </WithModal>
+          <ListAction
+            variant="download"
+            style="primary"
+            icon="download"
+            href={`http://localhost:4500/engines/bin/${props.engine}/${props.id}`}
+            filename={props.engine}
+          />
+        </ListActions>
+      </ListRow>
+    </>
+  )
+}
