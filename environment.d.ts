c252b092a882fa84a7e83af0c39e9167ea16be9e
Henrik Thoroe <henrik@thoroee.com>
2023-10-18 13:41:59 +0200

    feat: add auth and refresh URLs

--- environment.d.ts
+++ environment.d.ts
@@ -8,5 +8,9 @@ declare namespace NodeJS {
     readonly REPLAYS_HOST: string
     readonly STATS_HOST: string
     readonly NEXT_PUBLIC_STATS_HOST: string
+    readonly AUTH_HOST: string
+    readonly NEXT_PUBLIC_AUTH_HOST: string
+    readonly REFRESH_URL: string
+    readonly NEXT_PUBLIC_REFRESH_URL: string
   }
 }
