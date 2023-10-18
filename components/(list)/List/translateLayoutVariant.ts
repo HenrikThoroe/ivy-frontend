600f7f5664491b6f654b02168be6509bd443cdde
Henrik Thoroe <henrik@thoroee.com>
2023-10-18 22:36:22 +0200

    fix(comp): return object map instead of string
    returning an interpolated string could break automatic CSS class optimization

--- components/(list)/List/translateLayoutVariant.ts
+++ components/(list)/List/translateLayoutVariant.ts
@@ -4,8 +4,14 @@ export type LayoutVariant = 'custom-1' | 'custom-2' | 'custom-3' | 'custom-4' | 
  * Translates a layout variant to a Tailwind CSS class defining the number of grid columns.
  *
  * @param variant The layout variant to translate.
- * @returns The Tailwind CSS class.
+ * @returns An object with a single key-value pair, where the key is the Tailwind CSS class and the value is `true`.
  */
 export function translateLayoutVariant(variant: LayoutVariant) {
-  return `grid-cols-${variant}`
+  return {
+    'grid-cols-custom-1': variant === 'custom-1',
+    'grid-cols-custom-2': variant === 'custom-2',
+    'grid-cols-custom-3': variant === 'custom-3',
+    'grid-cols-custom-4': variant === 'custom-4',
+    'grid-cols-custom-5': variant === 'custom-5',
+  }
 }
