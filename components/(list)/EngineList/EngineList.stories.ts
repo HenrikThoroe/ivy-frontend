3526e6f95059642b5770d446d81c4a4e89e5b4a0
Henrik Thoroe <henrik@thoroee.com>
2023-10-19 13:50:05 +0200

    docs(story): add story for `EngineList`

---
+++ components/(list)/EngineList/EngineList.stories.ts
@@ -0,0 +1,80 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { EngineConfig } from '@ivy-chess/model'
+import EngineList from './EngineList'
+
+const meta: Meta<typeof EngineList> = {
+  tags: ['autodocs'],
+  title: 'Components/Lists/Engine List',
+  component: EngineList,
+}
+
+type Story = StoryObj<typeof EngineList>
+
+const sample: EngineConfig = {
+  name: 'Test 1',
+  variations: [
+    {
+      version: {
+        major: 1,
+        minor: 0,
+        patch: 0,
+      },
+      flavours: [
+        {
+          arch: 'amd64',
+          os: 'linux',
+          capabilities: [],
+          id: '1',
+        },
+        {
+          arch: 'arm64',
+          os: 'linux',
+          capabilities: [],
+          id: '2',
+        },
+        {
+          arch: 'arm64',
+          os: 'linux',
+          capabilities: ['ASIMD', 'FMA'],
+          id: '3',
+        },
+      ],
+    },
+    {
+      version: {
+        major: 2,
+        minor: 0,
+        patch: 1,
+      },
+      flavours: [
+        {
+          arch: 'amd64',
+          os: 'windows',
+          capabilities: ['AVX2', 'AVX512F', 'SSE4_1', 'SSE4_2'],
+          id: '4',
+        },
+        {
+          arch: 'arm64',
+          os: 'linux',
+          capabilities: [],
+          id: '5',
+        },
+        {
+          arch: 'arm64',
+          os: 'darwin',
+          capabilities: ['ASIMD', 'FMA'],
+          id: '6',
+        },
+      ],
+    },
+  ],
+}
+
+export const Primary: Story = {
+  args: {
+    engine: sample,
+  },
+}
+
+export default meta
