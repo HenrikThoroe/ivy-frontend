90af454e70403b982a5c04479f3362d3f6279d5b
Henrik Thoroe <henrik@thoroee.com>
2023-10-19 19:53:01 +0200

    fix(lib): validate `params` for null

--- lib/hooks/useReplayFilterOptions.ts
+++ lib/hooks/useReplayFilterOptions.ts
@@ -1,12 +1,17 @@
 import { useSearchParams } from 'next/navigation'
-import { ReplayClient } from '../api/clients/ReplayClient'
+import { ReplayClient, ReplayFilterOptions } from '../api/clients/ReplayClient'
 
 /**
  * Hook to parse the URL query parameters into {@link ReplayFilterOptions}.
  *
  * @returns The parsed {@link ReplayFilterOptions}.
  */
-export function useReplayFilterOptions() {
+export function useReplayFilterOptions(): ReplayFilterOptions {
   const params = useSearchParams()
+
+  if (!params) {
+    return {}
+  }
+
   return ReplayClient.parseReplayFilterOptions(params)
 }
