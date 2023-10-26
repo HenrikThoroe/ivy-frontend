8cc5c0d04a57062ea64ae99685ea6f444a84a10f
Henrik Thoroe <henrik@thoroee.com>
2023-10-26 11:09:23 +0200

    feat(comp): remove space between value and unit

--- lib/util/format.ts
+++ lib/util/format.ts
@@ -13,7 +13,7 @@ export function formatMemSize(bytes: number, decimals: number = 2) {
   const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
   const i = Math.floor(Math.log(bytes) / Math.log(k))
 
-  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(decimals))} ${units[i]}`
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(decimals))}${units[i]}`
 }
 
 /**
