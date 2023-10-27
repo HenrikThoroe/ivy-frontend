a0072d998516e2f8a2a4d3863d0328af95697e68
Henrik Thoroe <henrik@thoroee.com>
2023-10-27 09:07:27 +0200

    feat(comp): add dedicated card for `EngineTestConfig`s

---
+++ components/(card)/EngineTestConfigCard/EngineTestConfigCard.stories.tsx
@@ -0,0 +1,43 @@
+import { EngineTestConfig } from '@ivy-chess/model'
+import type { Meta, StoryObj } from '@storybook/react'
+import EngineTestConfigCard from './EngineTestConfigCard'
+
+const meta: Meta<typeof EngineTestConfigCard> = {
+  tags: ['autodocs'],
+  title: 'Components/Card/Engine Test Config',
+  component: EngineTestConfigCard,
+  decorators: [
+    (Story) => (
+      <div style={{ width: '100%', display: 'flex', padding: '2rem', flexDirection: 'column' }}>
+        <Story />
+      </div>
+    ),
+  ],
+}
+
+const engine: EngineTestConfig = {
+  name: 'Bar',
+  version: {
+    major: 2,
+    minor: 3,
+    patch: 1,
+  },
+  options: {
+    hash: 7176,
+    threads: 32,
+  },
+  timeControl: {
+    type: 'movetime' as const,
+    value: 10000,
+  },
+}
+
+type Story = StoryObj<typeof EngineTestConfigCard>
+
+export const Plain: Story = {
+  args: {
+    engine,
+  },
+}
+
+export default meta
---
+++ components/(card)/EngineTestConfigCard/EngineTestConfigCard.tsx
@@ -0,0 +1,41 @@
+import { formatMemSize } from '@/lib/util/format'
+import { EngineTestConfig, encodeVersion } from '@ivy-chess/model'
+
+interface Props {
+  /**
+   * Engine test configuration
+   */
+  engine: EngineTestConfig
+}
+
+/**
+ * A card to display information about an engine test configuration.
+ */
+export default function EngineTestConfigCard({ engine }: Props) {
+  //* UI
+
+  const Entry = ({ label, value }: { label: string; value: string }) => {
+    return (
+      <div className="flex w-full flex-col gap-2">
+        <span className="text-sm font-medium text-on-primary-light">{label}</span>
+        <span className="pl-2 text-base font-bold text-on-primary">{value}</span>
+      </div>
+    )
+  }
+  //* Render
+
+  return (
+    <section className="flex w-max min-w-[20rem] flex-col items-center justify-center gap-8 rounded-md px-8 py-6 shadow-card">
+      <span className="bg-gradient-to-br from-indigo-300 to-sky-600 bg-clip-text text-4xl font-extrabold text-transparent">
+        {engine.name}
+      </span>
+      <div className="flex w-full flex-col gap-6">
+        <Entry label="Version" value={encodeVersion(engine.version, false)} />
+        <Entry label="Threads" value={engine.options.threads.toString()} />
+        <Entry label="Hash Size" value={formatMemSize(engine.options.hash * 1000 * 1000, 0)} />
+        <Entry label="Time Control" value={engine.timeControl.type} />
+        <Entry label="Control Value" value={engine.timeControl.value.toString()} />
+      </div>
+    </section>
+  )
+}
