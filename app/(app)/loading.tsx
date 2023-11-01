e2bb75cdc3be6578030b3f9e830f0ff006b9c4d5
Henrik Thoroe <henrik@thoroee.com>
2023-11-01 12:37:33 +0100

    feat(app): add simple loading page

---
+++ app/(app)/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex w-full items-center justify-center py-40">
+      <div className="h-8 w-8 animate-spin rounded-full border-b-2 border-t-2 border-action-primary" />
+    </div>
+  )
+}
