3b5fc7f873b165082c389eb80e106e757a19bc20
Henrik Thoroe <henrik@thoroee.com>
2023-11-06 09:24:05 +0100

    style(comp): remove whitespace in class list

--- components/(form)/(atoms)/LabeledInput/LabeledInput.tsx
+++ components/(form)/(atoms)/LabeledInput/LabeledInput.tsx
@@ -27,7 +27,7 @@ interface Props {
  */
 export default function LabeledInput(props: Props) {
   return (
-    <div className="flex w-full flex-col items-baseline gap-3 text-on-primary ">
+    <div className="flex w-full flex-col items-baseline gap-3 text-on-primary">
       <div className="flex flex-row gap-1">
         <span className="text-base font-medium">{props.label}</span>
         {props.required && <span className="text-action-invalid">*</span>}
