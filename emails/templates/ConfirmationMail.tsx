4a68694d2a88f42dc5f49f0f26f67e4dcf01383c
Henrik Thoroe <henrik@thoroee.com>
2023-11-15 20:32:36 +0100

    feat(mail): update icon URL

--- emails/templates/ConfirmationMail.tsx
+++ emails/templates/ConfirmationMail.tsx
@@ -33,14 +33,14 @@ export default function ConfirmationMail(props: Props) {
           <Container className="mx-auto my-[40px] w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
             <Section className="mt-[32px]">
               <Img
-                src={`${host}/ivy-logo.png`}
+                src={`${host}/apple-icon.png`}
                 width="40"
                 height="37"
                 alt="Vercel"
                 className="mx-auto my-0"
               />
             </Section>
-            <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">
+            <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-bold text-black">
               Welcome!
             </Heading>
             <Text className="text-[14px] leading-[24px] text-black">
